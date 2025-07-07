73e4266bce6b2cb4bd5ff30cf573fff7cd629c09
Patrícia Silva <prrs89@gmail.com>
2025-07-07 23:03:51 +0100

    [2 Java Fundamentals] 2.20 Exercise: JavaDoc

---
+++ src/_2javafundamentals/_2javaprogrammingbasics/_20javadoc/JavaDocExample.java
@@ -0,0 +1,19 @@
+package _2javafundamentals._2javaprogrammingbasics._20javadoc;
+
+/**
+ * The first line should be a 1 lne description of what the class is
+ *
+ * @author Patrícia Silva
+ */
+public class JavaDocExample {
+    /**
+     * Comment about what the method does
+     *
+     * @param string - Provide a simple description of what the parameter is
+     * @param number - Provide a simple description of what the parameter is
+     * @return - String Results
+     */
+    public String sampleMethod(String string, int number) {
+        return "Result";
+    }
+}
