6010827bed759825be81b46d16bdc4e050bddaed
Patrícia Silva <prrs89@gmail.com>
2025-07-07 22:55:37 +0100

    [2 Java Fundamentals] 2.18 Exercise: IntelliJ

---
+++ src/_2javafundamentals/_2javaprogrammingbasics/_18intellij/FirstClass.java
@@ -0,0 +1,7 @@
+package _2javafundamentals._2javaprogrammingbasics._18intellij;
+
+public class FirstClass {
+    public static void main(String[] args) {
+        System.out.println("My first Java Application");
+    }
+}
