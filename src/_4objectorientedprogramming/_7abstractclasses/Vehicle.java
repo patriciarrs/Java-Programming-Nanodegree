2df6943319710a4f1ecfc0f7f37722fe5c3e9d69
Patrícia Silva <prrs89@gmail.com>
2025-07-13 15:41:16 +0100

    [4 Object-Oriented Programming] 4.9 Exercise: Interfaces

--- src/_4objectorientedprogramming/_7abstractclasses/Vehicle.java
+++ src/_4objectorientedprogramming/_7abstractclasses/Vehicle.java
@@ -1,5 +1,16 @@
 package _4objectorientedprogramming._7abstractclasses;
 
+/**
+ * Can have class variables.
+ * Can have instance variables, i.e. variables that are specific to individual subclasses.
+ * Can have both abstract methods and concrete methods that are shared with the subclasses.
+ * Subclasses can only extend one class.
+ * <p>
+ * Use an ABSTRACT class when:
+ * We want subclasses to have instance variables.
+ * We want to provide concrete methods for subclasses.
+ * We want to make use of both concrete methods and abstract methods for subclasses.
+ */
 public abstract class Vehicle {
     private String start;
     private String stop;
---
+++ src/_4objectorientedprogramming/_9interfaces/Production.java
@@ -0,0 +1,5 @@
+package _4objectorientedprogramming._9interfaces;
+
+public interface Production {
+    public void location();
+}
