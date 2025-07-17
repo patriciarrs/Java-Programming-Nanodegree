4f806382b212ce2ec8ed3b97ca0766e6b1104a0f
Patrícia Silva <prrs89@gmail.com>
2025-07-17 16:46:05 +0100

    [6 Generics and Collections] 6.7 Exercise: Sorting Collections

---
+++ src/_6genericsandcollections/_7sortingcollections/StringSorting.java
@@ -0,0 +1,29 @@
+package _6genericsandcollections._7sortingcollections;
+
+import _5commontypes._5enums.Light;
+
+import java.util.Collections;
+import java.util.LinkedList;
+import java.util.List;
+
+public class StringSorting {
+    public static void main(String[] args) {
+        List<String> strings = new LinkedList<>();
+
+        strings.add("Zed");
+        strings.add("Alice");
+        strings.add("Mike");
+
+        for (String string : strings) {
+            System.out.println(string);
+        }
+
+        Collections.sort(strings);
+
+        System.out.println("-Sorted-");
+
+        for (String string : strings) {
+            System.out.println(string);
+        }
+    }
+}
