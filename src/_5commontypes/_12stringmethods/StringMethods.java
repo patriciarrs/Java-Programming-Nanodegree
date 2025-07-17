af4c5f8f865cc9a246581ffabac75426aee96a2e
Patrícia Silva <prrs89@gmail.com>
2025-07-17 13:38:52 +0100

    [5 Common Types] 5.12 String Methods

---
+++ src/_5commontypes/_12stringmethods/StringMethods.java
@@ -0,0 +1,25 @@
+package _5commontypes._12stringmethods;
+
+import java.util.Arrays;
+
+public class StringMethods {
+    public static void main(String[] args) {
+        String text = "Hello";
+
+        System.out.println(text.charAt(2));
+        System.out.println(text.equalsIgnoreCase("hello"));
+        System.out.println(text.contains("el"));
+
+        String commaSeparatedString = "This, is, a, comma, separated, list";
+
+        String[] stringArray = commaSeparatedString.split(",");
+
+        for (String string : stringArray) {
+            System.out.println(string);
+        }
+
+        System.out.println(commaSeparatedString.substring(0, 4));
+
+        System.out.println(text.replace("l", "2"));
+    }
+}
