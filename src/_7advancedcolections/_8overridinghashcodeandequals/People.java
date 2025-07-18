e13b2daa58fa354ce5ce10046fea2b00539ec717
Patrícia Silva <prrs89@gmail.com>
2025-07-18 17:26:28 +0100

    [7 Advanced Collections] 7.8 Exercise: Overriding hashcode and equals

---
+++ src/_7advancedcolections/_8overridinghashcodeandequals/People.java
@@ -0,0 +1,34 @@
+package _7advancedcolections._8overridinghashcodeandequals;
+
+import java.util.Objects;
+
+public class People {
+    private String address;
+    private String name;
+    private String number;
+    
+    /**
+     * Regarding equals() and hashCode(), when we override one, we must always override the other.
+     */
+
+    /**
+     * Define how to determine whether two objects are equal.
+     */
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        People people = (People) o;
+        return Objects.equals(address, people.address);
+    }
+
+    /**
+     * Ensures that each object has a unique identifier for efficient storage and retrieval.
+     * When inserting objects into a hash structure (like a HashMap, HashSet, or any other collection that utilizes hashing),
+     * this method helps in efficient storage and retrieval.
+     */
+    @Override
+    public int hashCode() {
+        return Objects.hashCode(address);
+    }
+}
