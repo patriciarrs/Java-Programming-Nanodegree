15053f4cb6b6227f78af2889e8cb1b30047caf54
Patrícia Silva <prrs89@gmail.com>
2026-09-07 11:22:53 +0100

    [4 Java Application Deployment]
    [5 Unit Testing With Java] 15 Exercise: Code Coverage

--- src/_4javaapplicationdeployment/_5unittestingwithjava/_10additionalunittesttopics/cart/src/test/java/com/udacity/cart/service/TotalsWithDiscountCalculatorTest.java
+++ src/_4javaapplicationdeployment/_5unittestingwithjava/_10additionalunittesttopics/cart/src/test/java/com/udacity/cart/service/TotalsWithDiscountCalculatorTest.java
@@ -40,10 +40,6 @@ class TotalsWithDiscountCalculatorTest {
         totalsWithDiscountCalculator = new TotalsWithDiscountCalculator(globalUser);
     }
 
-    // Replace this with a Repeated test. Use a BeforeAll method to create a user whose credit
-    // will be reduced with each repetition, and use a BeforeEach method to create a new
-    // TotalsWithDiscountCalculator
-    // for each repetition.
     @RepeatedTest(3)
     public void totalsWithDiscount_getTotals_reducesUserCredit(RepetitionInfo repetitionInfo) {
         totalsWithDiscountCalculator.getTotals(
@@ -51,9 +47,6 @@ class TotalsWithDiscountCalculatorTest {
         assertEquals(100.0 - 20 * repetitionInfo.getCurrentRepetition(), globalUser.getCredit());
     }
 
-    // Replace this with a parameterized test that uses a MethodSource to provide
-    // a stream of arguments allowing you to test both regular and platinum users with the
-    // same test.
     @ParameterizedTest
     @MethodSource("differentUserTypesAndExpectedTotals")
     public void totalsWithDiscounts_regularAndPlatinumUser_returnsDifferentSubtotal(
@@ -63,5 +56,12 @@ class TotalsWithDiscountCalculatorTest {
         CartItem item = new CartItem("Ten Dollar Item", 10.0, 1.0);
 
         assertEquals(expectedCartTotals, calculator.getTotals(List.of(item)));
+    }
+
+    @Test
+    void getTotalsWithDiscounts_userWithCredit_creditLessThanSubtotal() {
+        TotalsWithDiscountCalculator calculator = new TotalsWithDiscountCalculator(new User("User with a lot of credit", UserType.REGULAR, 15.0));
+        CartTotals totals = calculator.getTotals(List.of(new CartItem("Twenty dollar item", 20.0, 0)));
+        assertEquals(new CartTotals(5.0, 0), totals);
     }
 }
